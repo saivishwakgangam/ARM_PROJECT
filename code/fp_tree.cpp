9e239f121f17caf7f0bfd7a0cfb48f130c3194f6
saivishwakgangam <gangamsaivishwak@gmail.com>
2021-11-12 22:09:13 +0530

    final commit

--- code/fp_tree.cpp
+++ code/fp_tree.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+int s_c=360;
 class Node{
 public:int item_number;
        int count;
@@ -14,14 +15,14 @@ public:int item_number;
        }
 };
 class FP_TREE {
-    vector<int> transactions[59601];
+    vector<vector<int>>transactions;
     Node *root;
     map<int, int> item_set;
     vector<int> item_set_freq;
     map<int, Node *> item_header;
     int support_count;
     int transactions_size;
-
+    map<int,int>item_support_count;
 public:
     FP_TREE(int s_count) {
         root = new Node(-1);
@@ -33,22 +34,24 @@ public:
         cout << "READING DATASET....." << endl;
         ifstream read_file(file_name);
         map<int, int> temp_item_set;
-        vector<int> temp_transactions[59601];
+        vector<vector<int>>temp_transactions;
         string line;
         int index = 0;
         while (getline(read_file, line)) {
             stringstream ss(line);
             string token;
+            vector<int>t;
             while (getline(ss, token, ' ')) {
                 if (stoi(token) != -1 && stoi(token) != -2) {
                     temp_item_set[stoi(token)]++;
-                    temp_transactions[index].push_back(stoi(token));
+                    t.push_back(stoi(token));
 
                 }
 
             }
+            temp_transactions.push_back(t);
 
-            index++;
+
         }
         cout << "REMOVING ITEMS WITHOUT MINIMUM SUPPORT COUNT..." << endl;
         for (auto ele: temp_item_set) {
@@ -60,26 +63,72 @@ public:
         bool flag;
         for (auto ele: temp_transactions) {
             flag = false;
+            vector<int>t;
             for (auto e: ele) {
                 if (item_set.count(e)) {
                     flag = true;
-                    transactions[index].push_back(e);
+                    t.push_back(e);
                 }
             }
-            sort(transactions[index].begin(), transactions[index].end());
+            sort(t.begin(), t.end());
             if (flag) {
-                index++;
+                transactions.push_back(t);
             }
         }
-        transactions_size = index;
+        transactions_size = transactions.size();
         cout << "COMPLETED READING....ITEM COUNT IS " << item_set.size() << " TRANSACTION SIZE IS " << transactions_size
              << endl;
         read_file.close();
     }
+    void create_pattern_base(Node* n,int ele,int s_count) {
+        map<int, int> temp_item_set;
+        vector<int> temp_transactions[s_count+1];
+        //do suffix search
+        Node* temp=n;
+        int index=0;
+        while(temp){
+            //traverse temp up
+            Node* temp1=temp;
+            int min_s_count=temp1->count;
+            temp1=temp1->parent;
+            while(temp1->item_number!=-1){
+                temp_item_set[temp1->item_number]+=min_s_count;
+                temp_transactions[index].push_back(temp1->item_number);
+                temp1=temp1->parent;
+            }
+            //now copy temp transactions
+            for(int i=index+1;i<=index+min_s_count-1;i++){
+                temp_transactions[i]=temp_transactions[index];
+            }
+            index=index+min_s_count;
+            temp=temp->next_link;
+        }
+        for (auto ele: temp_item_set) {
+            if (ele.second >= support_count) {
+                item_set.insert(ele);
+            }
+        }
+        index = 0;
+        bool flag;
+        for (auto ele: temp_transactions) {
+            flag = false;
+            vector<int>t;
+            for (auto e: ele) {
+                if (item_set.count(e)) {
+                    flag = true;
+                    t.push_back(e);
+                }
+            }
+            sort(t.begin(), t.end());
+            if (flag) {
+                transactions.push_back(t);
+            }
+        }
+        transactions_size = transactions.size();
 
+    }
     void sort_transactions() {
-        cout << "SORTING THE ITEMSET ACCORDING TO THEIR FREQUENCY" << endl;
-        vector<pair<int, int>> temp_itemset;
+        vector<pair<int, int>>temp_itemset;
         for (auto ele: item_set) {
             temp_itemset.push_back({ele.second, ele.first});
         }
@@ -88,8 +137,9 @@ public:
             item_set_freq.push_back(ele.second);
             item_header[ele.second] = NULL;
         }
+
         reverse(item_set_freq.begin(), item_set_freq.end());
-        cout << "SORTING THE TRANSACTIONS...." <<endl;
+
         for (int i = 0; i < transactions_size; i++) {
 
             vector<int> temp_vector;
@@ -123,13 +173,15 @@ public:
             Node *head = item_header[element];
             if(head==NULL){
                 head=new_node;
-                head->next_link=NULL;
+                item_header[element]=head;
+
             }
             else{
                 while (head->next_link != NULL) {
                     head = head->next_link;
                 }
                 head->next_link = new_node;
+
             }
             //updating parent
             new_node->parent=root;
@@ -147,14 +199,12 @@ public:
         }
         return search(root->children[element],index+1,n,list);
     }
-    void construct_fp_tree() {
-        cout << "CONSTRUCTING FP TREE"<<endl;
+    Node* construct_fp_tree() {
         //parse the transactions
         for (int i = 0; i < transactions_size; i++) {
             root = insert_item_list(root, 0, transactions[i].size(), transactions[i]);
         }
         //checking the code
-        cout<<"TESTING FP TREE"<<endl;
         bool result=true;
         for(int i=0;i<transactions_size;i++){
             result&=search(root,0,transactions[i].size(),transactions[i]);
@@ -163,17 +213,226 @@ public:
             }
         }
         if(result){
-            cout<<"ALL TRANSACTIONS PRESENT";
+
         }
         else{
-            cout<<"ALL TRANSACTIONS NOT PRESENT";
+
+            return NULL;
+        }
+        return root;
+    }
+    vector<int> getItems(){
+        return item_set_freq;
+    }
+    int get_minimum_support_count(){
+        return item_set[*(item_set_freq.end()-1)];
+    }
+    int get_minimum(vector<int>t){
+        int minimum=INT_MAX;
+        for(auto ele:t){
+            minimum=min(minimum,item_set[ele]);
+        }
+        return minimum;
+    }
+    pair<int,Node*> get_item_header(int ele){
+        return {item_set[ele],item_header[ele]};
+    }
+    bool isEmpty(){
+        if(root->children.size()==0){
+            return true;
+        }
+        return false;
+    }
+    void delete_path(Node* ptr){
+        //update item_header table
+        if(ptr==NULL){
+            return;
+        }
+        Node *temp=ptr;
+        Node *temp1=temp->parent;
+        while(temp1->item_number!=-1){
+            if(temp1->count==temp->count){
+                item_header[temp1->item_number]=temp1->next_link;
+                temp1=temp1->parent;
+
+            }
+            else{
+                break;
+            }
         }
     }
+    int get_support_count(int item){
+        return item_set[item];
+    }
+    ~FP_TREE(){
+
+    }
+
 };
-int main(){
-    FP_TREE fp(100);
-    fp.read_dataset("dataset.txt");
-    fp.sort_transactions();
-    fp.construct_fp_tree();
+bool containsSinglePath(Node *root){
+    if(root->children.size()==0){
+        return true;
+    }
+    if(root->children.size()>=2){
+        return false;
+    }
+    return containsSinglePath(root->children.begin()->second);
+}
+void generate_subsets(int i,int size,vector<pair<int,int>>&items_seen,vector<int>&temp,vector<int>temp_b,FP_TREE *fp,vector<string>&closed_frequent_items){
+    if(i==size){
+        return;
+    }
+    //include the element at index i
+    temp_b.push_back(temp[i]);
+    // get minimum support count
+    int support_count=fp->get_minimum(temp_b);
+    string cfi="";
+    if(support_count>=s_c){
+
+        for(auto ele:items_seen){
+            cfi+= to_string(ele.first);
+            cfi+=" ";
+        }
+        for(auto ele:temp_b){
+            cfi+= to_string(ele);
+            cfi+=" ";
+        }
+        cfi+= to_string(support_count);
+    }
+    closed_frequent_items.push_back(cfi);
+    generate_subsets(i+1,size,items_seen,temp,temp_b,fp,closed_frequent_items);
+    //backtrack
+    temp_b.pop_back();
+    generate_subsets(i+1,size,items_seen,temp,temp_b,fp,closed_frequent_items);
+
+}
+void fp_growth(FP_TREE *fp,Node *root,vector<pair<int,int>>items_seen,vector<string>&closed_frequent_items){
+    //now check the base condition
+    if(containsSinglePath(root)){
+        //all the items present in header plus items seen are pushed to closed_frequent_items
+
+        vector<int>temp=fp->getItems();
+        vector<int>temp_b;
+        string cfi="";
+        int support_count=INT_MAX;
+        for(auto ele:items_seen){
+            support_count=min(support_count,ele.second);
+        }
+        if(support_count>=s_c){
+            for(auto ele:items_seen){
+                cfi+= to_string(ele.first);
+                cfi+=" ";
+            }
+            cfi+= to_string(support_count);
+        }
+        closed_frequent_items.push_back(cfi);
+        generate_subsets(0,temp.size(),items_seen,temp,temp_b,fp,closed_frequent_items);
+        return;
+    }
+    else{
+        //so tree contains more than one path shit!!!
+        //create conditional pattern bases for each element in header table
+        vector<int>items;
+        items=fp->getItems();
+        //got the items in reverse order of frequency so traverse from last to first(item header)
+        for(int i=items.size()-1;i>=0;i--){
+            //construct conditional pattern base and conditional pattern tree on items[i]
+            FP_TREE *fp_mine=new FP_TREE(s_c);
+            pair<int,Node*>p=fp->get_item_header(items[i]);
+            fp_mine->create_pattern_base(p.second,items[i],p.first);
+            //delete the path which is already accessed
+            //fp->delete_path(p.second);
+            fp_mine->sort_transactions();
+
+            Node *root_mine=fp_mine->construct_fp_tree();
+            if(fp_mine->isEmpty()){
+
+                continue;
+            }
+            else{
+                //recursive call to fp growth
+                items_seen.push_back({items[i],fp->get_support_count(items[i])});
+                fp_growth(fp_mine,root_mine,items_seen,closed_frequent_items);
+                delete fp_mine;
+                items_seen.pop_back();
+            }
+
+
+        }
+
+    }
+}
+bool isSubset(vector<int>v1,vector<int>v2){
+    int ptr1=0;
+    int ptr2=0;
+    while(ptr1<v1.size()&&ptr2<v2.size()){
+        if(v1[ptr1]>v2[ptr2]){
+            return false;
+        }
+        else if(v1[ptr1]<v2[ptr2]){
+            ptr1++;
+        }
+        else{
+            ptr1++;
+            ptr2++;
+        }
+    }
+    return (ptr2==v2.size());
+}
+int main() {
+    FP_TREE *fp = new FP_TREE(s_c);
+    fp->read_dataset("dataset.txt");
+    fp->sort_transactions();
+    Node *root = fp->construct_fp_tree();
+    vector<pair<int,int>> s;
+    vector<string> closed_frequent_items;
+    fp_growth(fp, root, s, closed_frequent_items);
+    delete fp;
+    cout<<"FREQUENT ITEMS ARE"<<endl;
+    for (auto ele: closed_frequent_items) {
+        cout << ele << endl;
+    }
+    //finding closed frequent itemsets
+    map<int,map<int,vector<int>>>mp;
+    for(auto ele:closed_frequent_items){
+        stringstream  ss(ele);
+        string token;
+        vector<int>t;
+        while(getline(ss,token,' ')){
+            t.push_back(stoi(token));
+        }
+        int support_count=t.back();
+        t.pop_back();
+        sort(t.begin(),t.end());
+        mp[support_count].insert({t.size(),t});
+    }
+    cout<<"FINDING CLOSED FREQUENT ITEMSETS"<<endl;
+    vector<vector<int>>closed_frequent;
+    for(auto it=mp.begin();it!=mp.end();it++){
+        map<int,vector<int>>frequent_items=it->second;
+        for(auto it1=frequent_items.rbegin();it1!=frequent_items.rend();it1++){
+            bool flag=false;
+            for(auto it2=frequent_items.rbegin();it2!=it1;it2++){
+                //use the 2 pointer concept
+                if(isSubset(it2->second,it1->second)){
+                    flag=true;
+                    break;
+                }
+            }
+            if(flag==false){
+                closed_frequent.push_back(it1->second);
+            }
+
+        }
+
+    }
+    cout<<"CLOSED FREQUENT ITEMSETS ARE"<<endl;
+    for(auto ele:closed_frequent){
+        for(auto e:ele){
+            cout<<e<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
+
 }
--- report.md
+++ report.md
@@ -1,26 +1,30 @@
 # Apriori
-## dataset stats
-The dataset used for this experiment contains clickstrem data from an e-commerce. In this dataset, the sequemce count is 59,601 and item count is 497.
+## Dataset Stats
+Dataset Name: BMSWEBVIEW1
+The dataset used for this experiment contains click stream data from an e-commerce. In this dataset, the sequence count is 59,601 and item count is 497.
 
 
 ## Hash Based Technique
 ### Implementation
 Hash based technique is implemented only for 2-item sets as mentioned in the pdf.
 1) Hash table is created with size 31.
-2) When scanning the dataset for 1-itemsets ,we can direclty get 2-itemsets.
-3) For each 2-itemset, a hash values is computed with mod 31. Using the hash value, we can direclty push itemset to respective bucket.
+2) When scanning the dataset for 1-itemsets ,we can directly get 2-itemsets.
+3) For each 2-itemset, a hash value is computed with mod 31. Using the hash value, we can directly push itemset to respective bucket.
 4) While finding frequent itemsets, we can ignore the buckets with count less than min support.
 5) From these candidate items, we can easily find itemsets with min support.
-6) By using hash table, The number of candidte items are decreased. So the efficiency is more than normal apriori.
+6) By using hash table, The number of candidate items are decreased. So the efficiency is more than normal apriori.
 
-## Run time analysis 
+## Run time analysis
 
 
 | Algorithm | With Out Hash table| With hash table | 
 | :---: | :---: | :---: | 
 | Candidate items count | 89681 | 63973 |
 
-We can observe that,these is significant decrease in number of candidate items. If we implement same for all itemsets, there will be significant improvement in hash based technique.
+
+1)We can observe that,there is significant decrease in number of candidate items. If we implement same for all itemsets, there will be significant improvement in run time with hash based technique.
+2)for 2-item sets, Difference in runtime for hash based and normal apriori is negligible.
+
 
 
 
@@ -28,15 +32,47 @@ We can observe that,these is significant decrease in number of candidate items. 
 
 ## Partitioning
 ### Dataset split
-The entire dataset is divide into some (tried different partitions ) partitons and basic apriori algorithm is run on each partiton sequentially. Once local frequent itemsets are found, the entire dataset is scanned again to find global frequent items sets. From these frequent global frequent items, Closed frequents itemsets are mined .
+The entire dataset is divide into some (tried different partitions ) partitions and basic apriori algorithm is run on each partition sequentially. Once local frequent itemsets are found, the entire dataset is scanned again to find global frequent items sets. From these frequent global frequent items, Closed frequents itemsets are mined .
 
 
 ### Implementation
-1) For each partition, algorithm scans all transactions to count number of occurences of each item. The items with count less than minimum support are discarded. 
-2) From frequent 1-itemsets, 2-items sets are computed by 
-finding combiantions of all items. Algorithm scans all transactions again to count all occurences of each 2-item.
-The items with count less than minimum support are discarded as done above.
-3) Step 2 is repeated till there are no k-items sets with count atleast equal to min support. Then k-1-item sets are returned. These are closed frequent items for each partition.
+1) For each partition, algorithm scans all transactions to count number of occurences of each item. The items with count less than minimum support are discarded.
+2) From frequent 1-item sets, 2-items sets are computed by
+   finding combinations of all items. Algorithm scans all transactions again to count all occurences of each 2-item.
+   The items with count less than minimum support are discarded as done above.
+3) Step 2 is repeated till there are no k-items sets with count at least equal to min support. Then k-1-item sets are returned. These are closed frequent items for each partition.
 4) Once all local frequent item sets are computed, global frequent itemsets are found by scanning entire dataset again.
 
-## Run time analysis 
+## Run time analysis
+Partition count=20
+
+| Min Support | 0.1 | 0.05  | 0.01 |
+| :---: | :---: | :---: | :---: | 
+| Run Time | 1 sec | 3 sec | 30 sec |
+
+We can observe that with decreasing min support ,run time is increasing. In this experiment, each partition was run using normal apriori.
+we observed runtime using apriori library and it has nearly same runtime.
+
+
+# FP GROWTH
+
+### Dataset Stats
+The dataset used for this experiment contains click stream data from an e-commerce. In this dataset, the sequence count is 59,601 and item count is 497.
+
+### ALGORITHM(FP TREE CONSTRUCTION)
+- Initially we read the database and individual count of each item is calculated. Now after observing the count values
+  we decided the minimum support count to be 360 and minimum support is 0.006.
+- Now items are sorted based on their frequency from highest to lowest and each transaction is sorted in the order of item
+  sets.
+- After sorting the transactions,fp(frequent pattern) tree is constructed maintaining node count,next link(for mining purpose),and parent(for mining purpose) for each node.
+
+### ALGORITHM(FP TREE MINING)
+- Now after construction of FP Tree FP growth algorithm is implemented on constructed FP Tree.
+- Each path is mined recursively for this we followed text book algorithm.
+- Then we calculated frequent item sets, from frequent item sets we calculated closed frequent item sets.
+
+### RUNTIME ANALYSIS
+- For a data with item count 129 after removing items with less support count and number of transactions 50718 it took around
+  3 sec to generate closed frequent item sets and we observed runtime using fp growth library and it has nearly same runtime.
+
+
